4f9bbac8336ecec8e732f18cfccb41befa80f4b2
Kranthikumar2369 <kranthikumar1111111@gmail.com>
2026-07-23 14:36:18 +0530

    First Commit

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -1,17 +1,16 @@
 import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
 import Product from "@/models/Product";
-
 import { getAuth } from "@clerk/nextjs/server";
-import { v2 as cloudinary } from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 import { NextResponse } from "next/server";
-
 
 //Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+
 })
 
 export async function POST(request) {
@@ -22,9 +21,10 @@ export async function POST(request) {
         const isSeller = await authSeller(userId)
 
         if (!isSeller) {
-            return NextResponse.json({success: false, message: 'not authorized'})
+            return NextResponse.json({ success: false, message: 'not authorized'})
             
         }
+
         const formData = await request.formData()
 
         const name = formData.get('name');
@@ -36,7 +36,7 @@ export async function POST(request) {
         const files = formData.getAll('images');
 
         if (!files || files.length === 0) {
-            return NextResponse.json({success: false, message: 'no files uploaded'})
+            return NextResponse.json({ success: false, message: 'no files uploaded'})
             
         }
 
@@ -45,7 +45,7 @@ export async function POST(request) {
                 const arrayBuffer = await file.arrayBuffer()
                 const buffer = Buffer.from(arrayBuffer)
 
-                return new Promise((resolve, reject) => {
+                return new Promise((resolve,reject)=>{
                     const stream = cloudinary.uploader.upload_stream(
                         {resource_type: 'auto'},
                         (error,result) => {
@@ -56,17 +56,17 @@ export async function POST(request) {
                             else{
                                 resolve(result)
                             }
-
                         }
                     )
 
                     stream.end(buffer)
+
                 })
-                
+
             })
         )
 
-        const image = result.map(result => result.secure_url)
+        const image = request.map(result => result.secure_url)
 
         await connectDB()
         const newProduct = await Product.create({
@@ -74,19 +74,17 @@ export async function POST(request) {
             name,
             description,
             category,
-            price: Number(price),
-            offerPrice: Number(offerPrice),
+            price:Number(price),
+            offerPrice:Number(offerPrice),
             image,
             date: Date.now()
-
         })
 
-        return NextResponse.json({success: true, message: 'upload successful', newProduct})
+        return NextResponse.json({ success:true, message: 'Upload successfull',newProduct})
         
-    } 
-    catch (error) {
-        return NextResponse.json({success: false, message: error.message})
-
-}
+    } catch (error) {
+        NextResponse.json({ success: false, message: error.message})
+        
+    }
     
 }

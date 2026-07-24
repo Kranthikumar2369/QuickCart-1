a38021a67d16d5eebb9f7710d2bf3b8bb0f9c213
Kranthikumar2369 <kranthikumar1111111@gmail.com>
2026-07-25 01:06:02 +0530

    First Commit

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -6,10 +6,11 @@ import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
-export async function GET(request){
+
+export async function GET(request) {
     try {
 
-        const {userId} = getAuth(request)
+        const{userId} = getAuth(request)
 
         await connectDB()
 
@@ -18,9 +19,10 @@ export async function GET(request){
 
         const orders = await Order.find({userId}).populate('address items.product')
 
-        return NextResponse.json({success: true, orders})
+        return NextResponse.json({success:true, orders})
         
     } catch (error) {
-        return NextResponse.json({success: false, message:error.message})
+        return NextResponse.json({success:false, message:error.message})
+        
     }
 }
